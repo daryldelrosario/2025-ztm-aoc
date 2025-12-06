7dbf1f592dd2dbe4427407e1cf0b5b43992a817d
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-06 07:03:49 -0500

    complete functions to read doc and reate array of start - end ranges

--- day02/day02-solution.js
+++ day02/day02-solution.js
@@ -1,1 +1,29 @@
-console.log("This is Day 2");
+import fs from 'fs';
+
+// GLOBAL VARIABLES
+const testInput = "day02-test-input.txt";
+
+// FUNCTION: Reading The Ranges Document
+function readTheDoc(path) {
+  return fs.readFileSync(path, 'utf8');
+}
+
+// FUNCTION: Separating The Ranges From Document
+function separateTheRanges(doc) {
+  const ranges = doc.split(",")
+
+  // creating the start and end points of each range
+    .map(range => {
+      const [start, end] = range.split("-").map(Number);
+      return {start, end };
+    });
+
+  return ranges;
+}
+
+// TESTING THE FUNCTIONS AND LOGIC
+const testRangeDoc = readTheDoc(testInput);
+const arrRange = separateTheRanges(testRangeDoc);
+
+// console.log(testRangeDoc);
+console.log(arrRange);

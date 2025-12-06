154e936dfbbfec05fb65ab21bc28e2a87fd6dbaf
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-05 20:48:08 -0500

    setup test paramenters

--- day01/day01-solution.js
+++ day01/day01-solution.js
@@ -1,1 +1,11 @@
-console.log("Hello Day 1");
+import fs from 'fs';
+
+function readFile(path) {
+  return fs.readFileSync(path, 'utf8');
+}
+
+const testInput = 'day01-test-input.txt';
+const puzzleInput = 'day01-input.txt';
+
+const testData = readFile(testInput);
+console.log(testData);

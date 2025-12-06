d188f28005bfa708370aeba7c3a9097884f02c72
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-06 15:04:14 -0500

    test import function

--- day03/day03-solution.js
+++ day03/day03-solution.js
@@ -1,4 +1,12 @@
 import { readDoc } from '../utils/input.js';
 
+// GLOBAL VARIABLES
 const testInput = "day03-testInput.txt";
 const puzzleInput = "day03-puzzleInput.txt";
+
+// FUNCTIONS
+
+// TESTING LOGIC
+const testDoc = readDoc(testInput);
+
+// console.log(testDoc);

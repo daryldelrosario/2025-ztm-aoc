efb875781e00f44f2f000ed2a3bb8eb859ab98a7
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-06 21:40:42 -0500

    update util input js file to have one master function to read and split doc inputs

--- utils/input.js
+++ utils/input.js
@@ -1,5 +1,11 @@
-import fs from 'fs';
+import fs from "fs";
 
-export function readDoc(path) {
-  return fs.readFileSync(path, 'utf8').trimEnd();
+export function getAndSplitDoc(path, delimiter = null) {
+  const text = fs.readFileSync(path, 'utf8').trim();
+
+  if(delimiter !== null) {
+    return text.split(delimiter);
+  }
+
+  return text.split(/\r?\n/);
 }

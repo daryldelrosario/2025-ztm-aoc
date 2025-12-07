0e4e272c742111e84072efa23903e9c24e30ae11
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-07 15:43:32 -0500

    set up and complete helper function to separate data input

---
+++ day05/day05_solution.js
@@ -0,0 +1,26 @@
+import { getAndSplitDoc } from "../utils/input.js";
+
+// GLOBAL VARIABLES
+const TEST_INPUT = getAndSplitDoc("day05_test-input.txt");
+const PUZZLE_INPUT = getAndSplitDoc("day05_puzzle-input.txt");
+
+// FUNCTIONS SECTION
+// FUNCTION: parse data to separate ranges and ids
+function parseData(data) {
+  const blankIndex = data.indexOf("");
+
+  const ranges = data.slice(0, blankIndex).map(range => {
+    const [start, end] = range.split("-").map(Number);
+    return {start, end };
+  });
+
+  const ids = data.slice(blankIndex + 1).map(Number);
+
+  return { ranges, ids };
+}
+
+// LOGIC TESTING
+console.log(parseData(TEST_INPUT));
+
+// console.log(TEST_INPUT);
+  

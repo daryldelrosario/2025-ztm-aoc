8ccda72c063ac541ac120876b62f2bb3e1f6fe84
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-06 21:55:55 -0500

    successfully create function to find max jolt in one bank line

--- day03/day03-solution.js
+++ day03/day03-solution.js
@@ -4,11 +4,30 @@ import { getAndSplitDoc } from '../utils/input.js';
 const testInput = "day03-testInput.txt";
 const puzzleInput = "day03-puzzleInput.txt";
 
-// FUNCTIONS
+// FUNCTION: Finding the Max Jolt in One Line of Bank
+function maxJoltOneBank(bank) {
+  let max = 0;
+
+  for (let i = 0; i < bank.length; i++) {
+    const first = Number(bank[i]);
+
+    for(let j = i + 1; j < bank.length; j++) {
+      const second = Number(bank[j]);
+      const value = (first * 10) + second;
+
+      if (value > max) {
+        max = value;
+      }
+    }
+  }
+
+  return max;
+}
 
 // TESTING LOGIC
 const testDoc = getAndSplitDoc(testInput);
 const actualDoc = getAndSplitDoc(puzzleInput);
 
+console.log(maxJoltOneBank("818181911112111"));
+// console.log(actualDoc);
 // console.log(testDoc);
-console.log(actualDoc);

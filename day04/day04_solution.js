31228ec1a3d4a728163ef4c03a640ad960dc60b7
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-07 07:16:53 -0500

    successfully create day 4 boilerplate

---
+++ day04/day04_solution.js
@@ -0,0 +1,8 @@
+import { getAndSplitDoc } from "../utils/input.js";
+  
+  // GLOBAL VARIABLES
+  const TEST_INPUT = getAndSplitDoc("day04_test-input.txt");
+  const PUZZLE_INPUT = getAndSplitDoc("day04_puzzle-input.txt");
+  
+  // LOGIC TESTING
+  console.log(TEST_INPUT);

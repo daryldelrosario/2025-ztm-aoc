dc77e702da29bb81f69f05a1c7018e576e177715
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-06 22:28:44 -0500

    successfully refactor maxjoltonebank function to allow user choice of 2 or 12

--- day03/day03-solution.js
+++ day03/day03-solution.js
@@ -4,7 +4,7 @@ import { getAndSplitDoc } from '../utils/input.js';
 const testInput = "day03-testInput.txt";
 const puzzleInput = "day03-puzzleInput.txt";
 
-// FUNCTION: Finding the Max Jolt in One Line of Bank
+// FUNCTION: Finding the Max Jolt in One Line of Bank - Good for Part 1
 function maxJoltOneBank(bank) {
   let max = 0;
 
@@ -36,12 +36,46 @@ function totalJolt(path) {
   return sum;
 }
 
+// REFACTORING FUNCTIONS maxJoltPerBank & totalJolt TO ALLOW FOR picks 2 - 12 (USER CHOICE)
+// FUNCTION: Finding Max Jolt Per Bank with User Choice of Picks
+function maxJoltPerBankUserChoice(bank, picks) {
+  const bankLength = bank.length;
+
+  if(bankLength <= picks) return Number(bank);
+
+  let result = "";
+  let start = 0;
+
+  for(let remaining = picks; remaining > 0; remaining--) {
+    const end = bankLength - remaining;
+
+    let bestDigit = "0";
+    let bestIndex = start;
+
+    for(let i = start; i <= end; i++) {
+      const currentDigit = bank[i];
+      if(currentDigit > bestDigit) {
+        bestDigit = currentDigit;
+        bestIndex = i;
+
+        if(bestDigit === "9") break;
+      }
+    }
+
+    result += bestDigit;
+    start = bestIndex + 1;
+  }
+
+  return Number(result);
+}
+
 // TESTING LOGIC
 const testDoc = getAndSplitDoc(testInput);
 const actualDoc = getAndSplitDoc(puzzleInput);
 
-console.log(`Test Input Sum: ${totalJolt(testInput)}`);
-console.log(`Actual Puzzle Sum: ${totalJolt(puzzleInput)}`);
+console.log(maxJoltPerBankUserChoice("234234234234278", 12));
+// console.log(`Test Input Sum: ${totalJolt(testInput)}`);
+// console.log(`Actual Puzzle Sum: ${totalJolt(puzzleInput)}`);
 // console.log(maxJoltOneBank("818181911112111"));
 // console.log(actualDoc);
 // console.log(testDoc);

e55efc87914d620e84caab7df61a62310bdec864
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-07 14:08:34 -0500

    complete function to locate all paper rolls

--- day04/day04_solution.js
+++ day04/day04_solution.js
@@ -4,9 +4,31 @@ import { getAndSplitDoc } from "../utils/input.js";
 const TEST_INPUT = getAndSplitDoc("day04_test-input.txt");
 const PUZZLE_INPUT = getAndSplitDoc("day04_puzzle-input.txt");
 
-// FUNCTIONS SECTION
+const NEIGHBOUR_OFFSETS = [
+  [-1, -1], [0, 1], [1, -1],
+  [-1,  0],         [1,  0],
+  [-1,  1], [0, 1], [1,  1],
+];
 
+// FUNCTIONS SECTION
+// FUNCTION: locating where the paper rolls are in the diagram they provide
+function locateRolls(diagram) {
+  const grid = diagram;
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  for(let row = 0; row < rows; row++) {
+    for(let col = 0; col < cols; col++) {
+      const cell = grid[row][col];
+
+      if(cell === "@") {
+        console.log(`Found @ at: [${row},${col}]`);
+      }
+    }
+  }
+}
 
 // LOGIC TESTING
 
-console.log(TEST_INPUT);
+locateRolls(TEST_INPUT);
+// console.log(TEST_INPUT);

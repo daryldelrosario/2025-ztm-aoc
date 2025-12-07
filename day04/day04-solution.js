b8065b6d0d0a3c9d2e5045e196d39ba4d22c5f6d
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-07 07:08:10 -0500

    removed old day 4 folder

--- day04/day04-solution.js
+++
@@ -1,7 +0,0 @@
-import { getAndSplitDoc } from '../utils/input.js';
-
-// GLOBAL VARIABLES
-const testInput = "day04-testInput.txt";
-const puzzleInput = "day04-puzzleInput.txt";
-
-// LOGIC TESTING

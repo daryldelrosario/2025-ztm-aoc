56e7ffb8182290dcefd337514dd98f298cc9c098
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-08 10:36:11 -0500

    setup day 6 challenge

---
+++ day06/day06_solution.js
@@ -0,0 +1,12 @@
+import { getAndSplitDoc } from "../utils/input.js";
+
+// GLOBAL VARIABLES
+const TEST_INPUT = getAndSplitDoc("day06_test-input.txt");
+const PUZZLE_INPUT = getAndSplitDoc("day06_puzzle-input.txt");
+
+// FUNCTIONS SECTION
+
+// LOGIC TESTING
+
+console.log(TEST_INPUT);
+  

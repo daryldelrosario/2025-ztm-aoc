f983b5307a0e91a476de35bfe2c94cdda9b1a5fc
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-12-09 09:28:46 -0500

    setup day 7

---
+++ day07/day07_solution.js
@@ -0,0 +1,12 @@
+import { getAndSplitDoc } from "../utils/input.js";
+
+// GLOBAL VARIABLES
+const TEST_INPUT = getAndSplitDoc("day07_test-input.txt");
+const PUZZLE_INPUT = getAndSplitDoc("day07_puzzle-input.txt");
+
+// FUNCTIONS SECTION
+
+// LOGIC TESTING
+
+console.log(TEST_INPUT);
+  
